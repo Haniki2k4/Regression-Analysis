2a28c313671f9a9f2e73652598cc3b24a6a76b20
haniki2k4 <haniki2k4@gmail.com>
2025-10-11 00:27:08 +0700

    add source code

---
+++ source_code_final_exam.R
@@ -0,0 +1,14 @@
+library(haven)      # đọc file .sav
+library(dplyr)      # xử lý dữ liệu
+library(ggplot2)    # trực quan hóa
+library(janitor)    # tabyl tần số
+library(tidyr)      # reshape dữ liệu
+library(scales)     # hiển thị %
+library(knitr)
+library(kableExtra)
+library(MASS)
+library(VIM)
+library(zoo)      # Cần cho việc nội suy na.approx()
+library(forecast) # Thư viện chính cho mô hình chuỗi thời gian (ARIMA, Holt)
+library(ggfortify)# Để vẽ đồ thị forecast đẹp hơn với ggplot2
+library(purrr)
